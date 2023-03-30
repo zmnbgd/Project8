8a858450e5d3e60c0468a1aeadc95080247b37ce
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-03-30 23:25:06 +0200

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -36,8 +36,17 @@ class ViewController: UIViewController {
         answersLabel.translatesAutoresizingMaskIntoConstraints=false
         answersLabel.font = UIFont.systemFont (ofSize: 24)
         answersLabel.text = "ANSWERS"
+        answersLabel.textAlignment = .right
         answersLabel.numberOfLines = 0
         view.addSubview(answersLabel)
+        
+        currentAnswer = UITextField ()
+        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+        currentAnswer.placeholder = "Tap letters to guess"
+        currentAnswer.textAlignment = .center
+        currentAnswer.font = UIFont.systemFont(ofSize: 44)
+        currentAnswer.isUserInteractionEnabled = false
+        view.addSubview(currentAnswer)
         
         NSLayoutConstraint.activate([
         scoreLabel.topAnchor.constraint (equalTo:
@@ -45,7 +54,25 @@ class ViewController: UIViewController {
         scoreLabel.trailingAnchor.constraint (equalTo:
         view.layoutMarginsGuide.trailingAnchor),
         // more constraints to be added here
+        
+        cluesLabel.topAnchor.constraint (equalTo: scoreLabel.bottomAnchor),
+        cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+        cluesLabel.widthAnchor.constraint (equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant:
+        -100),
+        answersLabel.topAnchor.constraint (equalTo: scoreLabel.bottomAnchor),
+        answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+        answersLabel.widthAnchor.constraint(equalTo:
+        view.layoutMarginsGuide.widthAnchor,multiplier: 0.4, constant: -100),
+        answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor),
+        
+        currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        //MARK: multiplier: 0.5) is half of the screen
+        currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+        currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20),
         ])
+        
+        cluesLabel.backgroundColor = .red
+        answersLabel.backgroundColor = . blue
     }
 
     override func viewDidLoad() {
@@ -54,4 +81,4 @@ class ViewController: UIViewController {
     }
 }
 
-//11:34
+//19:55
