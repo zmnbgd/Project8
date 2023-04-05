f885bb8b7263f7c3117f57b26a3024f49f6ba196
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-04-05 23:42:10 +0200

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -143,5 +143,33 @@ class ViewController: UIViewController {
     @objc func clearTapped (_ sender: UIButton) {
         
     }
-}
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionsString = ""
+        var letterBits = [String]()
 
+        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
+            if let levelContents = try? String(contentsOf: levelFileURL) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+                
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1).\(clue)\n"
+                    
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: " ")
+                    solutionsString += "\(solutionWord.count) letters \n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+    }
+}
+//12:35
